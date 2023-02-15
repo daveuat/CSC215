a667828e69972f42605a43ef1ca3e84e58800eaa
David G <davgrego@uat.edu>
2023-02-15 11:09:46 +0900

    Test!

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+// Main Function to start program.
+// Must have 1 but no more.
+int main()
+{
+	// Code block is defined by the curly braces.
+	// cout stands for console out.
+	// endl stands for end of line.
+	cout << "The lander has touched down on Mars!" << endl <<endl;
+	int numPets;
+	cout << "How many pets do you have? " << endl;
+	cin >> numPets;
+	cout << "You have " << numPets << ". That's a lot!" << endl;
+}
